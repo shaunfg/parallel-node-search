fc55afb6afa590a45c730b2caefcdc672c6da103
arkirat <69860764+arkirat@users.noreply.github.com>
2020-11-29 23:30:39 -0500

    functions for parallel split optimizer

--- local_search.jl
+++ local_search.jl
@@ -1,6 +1,8 @@
 include("./tree.jl")
 #input starting decision tree, training data X,y
 #output locally optimal decision tree
+using CSV, DataFrames, Random
+cd("/Users/arkiratanglertsumpun/Documents/GitHub/parallel-node-search")
 
 depth = 2
 T = tf.get_tree(depth)
@@ -12,28 +14,73 @@ y = Vector(iris[:,5])
 #--- 8.1 Local Search
 
 #function to calculate loss
-# function Loss(T,X,y):
-#     for leaves in T.leaves
-#     end
-# end
-
-#While loss is still decreasing
-
-#Randomize the nodes
-shuffled_t = shuffle(T.nodes)
-for t in shuffled_t
-    #get the indices of the nodes in the subtree and create Tree struct
-    I = tf.nodes_subtree(2,T)
-    Tt = tf.create_subtree(I,T)
-
-    #Optimize subtree
-    #To code in 8.2
-
-    #Replace the original tree with optimal subtree
-    #call on function: tf.replace_subtree
+function loss(T,X,y):
+    for leaves in T.leaves
+    end
 end
 
-#--- Optimize Node Parallel
+#While loss is still decreasing
+error_prev = loss(T,X,y)
+tol = 1e-4
+while abs(error_prev - error_cur) > tol #while improvements are still possible
+    #Randomize the nodes
+    shuffled_t = shuffle(T.nodes)
+    for t in shuffled_t
+        #get the indices of the nodes in the subtree and create Tree struct
+        I = tf.nodes_subtree(2,T)
+        Tt = tf.create_subtree(I,T)
 
+        #Optimize subtree
+        #XI =
+        #YI =
+        #Tt = optimize_node_parallel(Tt,XI,YI)
+
+        #Replace the original tree with optimal subtree
+        Tt = tf.replace_subtree(T,Tt)
+    end
+end
+
+#return T
+
+#--- Optimize Node Parallel
+# Input: Subtree T to optimize, training data X,y
+# Output: Subtree T with optimized parallel split at root
+function optimize_node_parallel(T::Tree,XI,YI)
+    root = minimum(T.nodes)
+    if root = Tt.branches #if the subtree is a branch get its children
+        Tl, Tu = left_child(root,T), right_child(root,T)
+    else #it is a leaf -> create new leaf nodes
+        Tl, Tu = new leaf nodes
+    end
+    error_best = loss(T,X,y)
+
+    #T_para, error_para =
+    ######
+end
 
 #--- Best Parallel Split
+# Input: Tl and Tu subtrees as children of new split, X and y
+# Output: Subtree with best parallel split at root, error of best tree
+
+function minleafsize(T::Tree)
+    ###
+end
+
+n = size(X,1)
+p = size(X,2)
+error_best = Inf
+for j in 1:p
+    values = sort(X[:,j])
+    for i in 1:n-1
+        b = 0.5*(values[i] + values[i+1])
+        # T = branch node....
+        if minleafsize(T) >= Nmin
+            error = loss(T,X,y)
+            if error < error_best
+                error_best = error
+                Tbest = T
+            end
+        end
+    end
+    return(Tbest, error_best)
+end
