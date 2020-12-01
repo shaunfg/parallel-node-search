0de2b1fa6412a242e1c278a06e1d1247024ab58b
arkirat <69860764+arkirat@users.noreply.github.com>
2020-12-01 00:09:13 -0500

    created loss function

--- local_search.jl
+++ local_search.jl
@@ -1,28 +1,70 @@
-include("./tree.jl")
+include("./tree_ls.jl")
 #input starting decision tree, training data X,y
 #output locally optimal decision tree
-using CSV, DataFrames, Random
+using CSV, DataFrames, Random, LinearAlgebra
+
 cd("/Users/arkiratanglertsumpun/Documents/GitHub/parallel-node-search")
 
 depth = 2
-T = tf.get_tree(depth)
+T = tf.get_randtree(depth,3)
 iris_full = CSV.read("iris.csv",DataFrame)
 iris = iris_full[randperm(size(iris_full,1)),:]#[1:3,:]
 x = Matrix(iris[:,1:4])
 y = Vector(iris[:,5])
 
+#----
+#Data pre-processing
+dt = fit(UnitRangeTransform, x, dims=1)
+x_copy = x
+x = StatsBase.transform(dt,x)
+Y = tf.y_mat(y) #matrix mapping observations and classes
 #--- 8.1 Local Search
 
+#random restart: initialize a random tree
+seed = 100
+Random.seed!(seed)
+
+m = length(T.branches) #num branches
+n = size(x,1) #num observations
+p = size(x,2) #num features
+b = rand(m,1) #randomize split thresholds for each branch
+e = 1*Matrix(I,p,p) #Identity matrix
+z = zeros(n,length(T.nodes)) #observations x leaf assignments
+a = zeros(p,m) #features being splitted in each branch
+
+# for j in 1:m
+#     i = rand(1:p)
+#     a[i,j] = 1
+# end
+
+#starting decision tree and assigments
+tf.assign_class(x,T,a,b,z,e)
+L =loss(T,Y,z)
+
 #function to calculate loss
-function loss(T,X,y):
-    for leaves in T.leaves
+function loss(T,Y,z)
+    L̂ = length(T.leaves)
+    classes = size(Y,2) #number of classes
+    Nt = zeros(length(T.nodes))
+    Nkt = zeros(classes,length(T.nodes))
+    Lt = zeros(length(T.nodes))
+    L = 0
+    for t in T.leaves
+        Nt[t] = sum(z[:,t])
+        for k in 1:classes
+            Nkt[k,t] = z[:,t]'*Y[:,k]
+        end
+        Lt[t] = Nt[t] - maximum(Nkt[:,t])
     end
+    L = sum(Lt)/L̂ #### + α*Cp......
+    return(L)
 end
 
 #While loss is still decreasing
-error_prev = loss(T,X,y)
-tol = 1e-4
-while abs(error_prev - error_cur) > tol #while improvements are still possible
+Lprev = loss(T,Y,z)
+tol = 100
+
+while tol > 1e-4 #while improvements are still possible
     #Randomize the nodes
     shuffled_t = shuffle(T.nodes)
     for t in shuffled_t
@@ -36,8 +78,11 @@ while abs(error_prev - error_cur) > tol #while improvements are still possible
         #Tt = optimize_node_parallel(Tt,XI,YI)
 
         #Replace the original tree with optimal subtree
-        Tt = tf.replace_subtree(T,Tt)
+        T = tf.replace_subtree(T,Tt)
+        tf.assign_class(x,T,a,b,z,e)
     end
+    Lcur = loss(T,Y,z)
+    tol = abs(Lprev - Lcur)
 end
 
 #return T
