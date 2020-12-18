300daf4d5b03a2199c6273e025e5f2449d44392e
arkirat <69860764+arkirat@users.noreply.github.com>
2020-12-17 20:29:41 -0500

    merge halftree

--- local_search_half.jl
+++ local_search_half.jl
@@ -52,11 +52,46 @@ function LocalSearch(x,y,tdepth,seed;tol_limit = 1,α=0.01)
     println("### Local Search Algorithm ###")
     println("##############################")
 
+    function HalfTreeSearch(T,X,y,tol_limit)
+        tol = 10
+        local iter = 0
+        while tol > tol_limit
+            iter +=1
+            println("------------- Iteration # $iter ----------------")
+            Lprev = loss(T,y)
+            local Lcur
+            local shuffled_t = shuffle(T.nodes)
+            print(shuffled_t)
+            for t in shuffled_t
+                if t ∈ T.nodes
+                    # println("STARTING TREE node $t-- $T")
+                    Tt = tf.create_subtree(t,T)
+                    # println("STARTING node $t-- $Tt")
+                    local indices = tf.subtree_inputs(Tt,X,y)
+                    # println(length(indices))
+                    Ttnew, better_found = optimize_node_parallel(Tt,indices,X,y,T,e)
+                    if better_found ==true
+                        global T_replacement = Ttnew
+                        T = replace_subtree(T,Ttnew,X;print_prog=true)
+                        global output = T
+                        # println("replaced Tree $t-- $T")
+                    end
+                    Lcur = loss(T,y)
+                    tol = abs(Lprev - Lcur)
+                    println("Lprev $Lprev, Lcur $Lcur")
+                end
+            end
+            println("Tolerance = $tol, Error = $Lcur, starting error = $starting_loss")
+        end
+        return T
+    end
+
     # Data pre-processing - Normalisation
     dt = fit(UnitRangeTransform, x, dims=1)
     X = StatsBase.transform(dt,x)
     e = tf.get_e(size(x,2))
     local T = tf.warm_start(tdepth,y,X,seed)
+<<<<<<< HEAD:local_search.jl
     global starting_tree = T
     starting_loss = loss(T,y,α)
     tol = 10
@@ -85,10 +120,35 @@ function LocalSearch(x,y,tdepth,seed;tol_limit = 1,α=0.01)
                 tol = abs(Lprev - Lcur)
                 println("Lprev $Lprev, Lcur $Lcur")
             end
+=======
+    global previous_tree = T
+    starting_loss = loss(T,y)
+
+    #Break the trees into left and right half: get nodes, subtrees, indices
+    global Tt_L = tf.create_subtree(2,T)
+    local L_indices = tf.subtree_inputs(Tt_L,X,y)
+
+    global Tt_R = tf.create_subtree(3,T)
+    local R_indices = tf.subtree_inputs(Tt_R,X,y)
+    numthreads = Threads.nthreads()-4
+
+    Threads.@threads for i in 1:numthreads
+        if i == 1
+            Tt_L = HalfTreeSearch(Tt_L,X,y,tol_limit)
+        else
+            Tt_R = HalfTreeSearch(Tt_R,X,y,tol_limit)
+>>>>>>> dev/halftree:local_search_half.jl
         end
-        println("Tolerance = $tol, Error = $Lcur, starting error = $starting_loss")
     end
-    return T
+    tol = 1e-10
+    T = replace_subtree(T,Tt_R,X;print_prog=true)
+    T = replace_subtree(T,Tt_L,X;print_prog=true)
+    println("FInal output tree $T")
+    global final_tree = T
+    Lcur = loss(T,y)
+    println("Lprev $starting_loss, Lcur $Lcur")
+
+    test_tree(T)
 end
 
 get_level(node,subtree_root) = Int(floor(log2(node/subtree_root)))
@@ -545,6 +605,7 @@ T_combined = replace_subtree(T,Ttnew,X)
 
 Thelp = tf.copy(T)
 append!(Thelp.branches ,[1000000000])
+
 
 Tlower_sub
 Tnew.branches
